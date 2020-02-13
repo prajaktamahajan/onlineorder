b40c0fcbd71ae600823ded5950ce3cbcc3d6c1f2
Mahajan <prajakta.mahajan@capgemini.com>
2020-02-13 14:15:39 +0530

    commited online order project

---
+++ onlineorder/src/main/java/com/order/onlineorder/OnlineorderApplication.java
@@ -0,0 +1,13 @@
+package com.order.onlineorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineorderApplication.class, args);
+	}
+
+}
---
+++ onlineorder/src/test/java/com/order/onlineorder/OnlineorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.onlineorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
